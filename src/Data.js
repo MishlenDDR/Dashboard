0c9aabe58c1d3a210a6aa240154585f23947dd52
unknown <hosteenj@student.21-school.ru>
2026-01-30 16:53:11 +0300

    done

---
+++ src/Data.js
@@ -0,0 +1,16 @@
+export const Icons = {
+        "13" : "/Snow.png",
+        "01" : "/Sunny.png",
+        "03" : "/Cloudy.png",
+        "02" : "/CloudSunny.png",
+        "04" : "/VeryCloudy.png",
+        "10" : "/Rain.png",
+    };
+export const TableOfWeather = {
+        "13" : "/SnowPic.png",
+        "01" : "/SunnyPic.png",
+        "04" : "/VeryCloudyPic.png",
+        "10" : "/RainPic.png",
+        "03" : "/CloudyPic.png",
+        "02" : "/CloudSunnyPic.png",
+}
